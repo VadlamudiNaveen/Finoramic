8149b0a162ef6099598a59d717bc4e7121e5a38c
Vadlamudi Naveen <39674418+VadlamudiNaveen@users.noreply.github.com>
2020-03-13 14:03:00 +0530

    Update anagrams.py

--- anagrams.py
+++ anagrams.py
@@ -1,3 +1,9 @@
+# algorithm
+# anagrams are those which are formed by having same letters but having a different order in every word.
+# so to find the like wise anagrams sort every string in the list and map them to a dictionary.
+# time complexity O(n)
+# space complexity size of dictionary. O(m).. {where m is number of keys}
+
 class Solution:
     def anagrams(self, data):
         temp={}
